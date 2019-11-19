4c31cd11132d85857faa08068f35ffce364256d1
Jeff Zivkovic <jzivkovic1611@gmail.com>
2019-11-19 15:07:14 -0700

    Completed app1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      text: ""
+    }
+
+    this.updateText = this.updateText.bind(this);
+  }
+
+  updateText(val){
+    this.setState({
+      text: val
+    });
+    console.log(this.state.text)
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <input onChange={ (event) => this.updateText(event.target.value) } />
+        <h1>{this.state.text}</h1>
       </div>
     );
   }
