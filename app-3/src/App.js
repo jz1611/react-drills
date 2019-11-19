9e402a19952e05c38dc6b0f4507c05fba4ce79a3
Jeff Zivkovic <jzivkovic1611@gmail.com>
2019-11-19 16:14:39 -0700

    Completed app3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,20 +1,30 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      userInput: "",
+      list: ['skis', 'snowboard', 'helmet', 'pants', 'jacket', 'boots'],
+      filtered: []
+    }
+  }
+
+  updateUserInputAndFiltered(value){
+    this.setState({
+      userInput: value,
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <input onChange={(event) => this.updateUserInputAndFiltered(event.target.value)} />
+        <ul>{this.state.list.filter(val => val.includes(this.state.userInput)).map((val, index) => <li key={index}>{val}</li>)}</ul>
       </div>
-    );
+    )
   }
 }
 
