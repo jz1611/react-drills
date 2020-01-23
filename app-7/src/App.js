397b669fbe2c561e194468bf61ce95fef55347ee
Jeff Zivkovic <jzivkovic1611@gmail.com>
2020-01-23 15:40:42 -0700

    Complete app7

--- app-7/src/App.js
+++ app-7/src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
 import "./App.css";
+import NewTask from "./NewTask";
+import List from "./List";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      list: []
+    };
+
+    this.addTask = this.addTask.bind(this);
+  }
+
+  addTask(task) {
+    this.setState({
+      list: [...this.state.list, task]
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        
+        <h1>To-Do List:</h1>
+        <NewTask add={this.addTask} />
+        <List tasks={this.state.list} />
       </div>
     );
   }
--- app-7/src/NewTask.js
+++ app-7/src/NewTask.js
@@ -1,5 +1,38 @@
 import React, { Component } from 'react';
 
 export default class NewTask extends Component {
-    
+    constructor() {
+      super();
+
+      this.state = {
+        input: ""
+      };
+
+      this.add=this.add.bind(this);
+    }
+
+    handleChange(value) {
+      this.setState({
+        input: value
+      });
+    }
+
+    add() {
+      this.props.add(this.state.input);
+      this.setState({
+        input: ""
+      });
+    }
+
+    render() {
+      return (
+        <div>
+          <input
+            value={this.state.input}
+            placeholder="Enter new task"
+            onChange={e => this.handleChange(e.target.value)} />
+          <button onClick={this.add}>Add</button>
+        </div>
+      );
+    }
 }
